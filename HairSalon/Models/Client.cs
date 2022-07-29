44e6f80692143e9fa5886903cbb9d96663ec95f3
Megan McKissack <meganmckissack@gmail.com>
2022-07-28 17:14:17 -0700

    create client model

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,10 @@
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+    }
+}
