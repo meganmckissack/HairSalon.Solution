dcb94f9c4cd62b87e3bdb81b8545f7652ae99bc1
Megan McKissack <meganmckissack@gmail.com>
2022-07-29 13:55:26 -0700

    update context model to include stylist db

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -6,6 +6,7 @@ namespace HairSalon.Models
     public class HairSalonContext : DbContext
     {
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Stylist> Stylists { get; set; }
 
         public HairSalonContext(DbContextOptions options) : base(options) { }
 
